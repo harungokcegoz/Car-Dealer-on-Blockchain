3268ee424b5d69bc1b6e568a63faa737809f6c0e
Harun Gokcegoz <harungokcegoz92@gmail.com>
2024-03-19 12:33:39 +0100

    mock up json is added to test the styling

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,9 @@
 import CarCardsGallery from '@/components/CarCardsGallery';
 
 const cars = [
-    { brand: 'Toyota', year: 2020, color: 'Red', askingPrice: 25000, imageUrl: '/toyota.jpg' },
-    { brand: 'Honda', year: 2019, color: 'Blue', askingPrice: 22000, imageUrl: '/honda.jpg' },
-    { brand: 'Ford', year: 2021, color: 'Black', askingPrice: 28000, imageUrl: '/ford.jpg' },
+    { brand: 'Toyota', year: 2020, color: 'Red', askingPrice: 25000, imageUrl: 'https://images.drive.com.au/driveau/image/upload/t_wp-default/v1/cms/uploads/itd0Lt3LTE6nvZcJgVNL' },
+    { brand: 'Honda', year: 2019, color: 'Blue', askingPrice: 22000, imageUrl: 'https://i0.shbdn.com/photos/17/59/91/x5_1110175991j9a.jpg' },
+    { brand: 'Ford', year: 2021, color: 'Black', askingPrice: 28000, imageUrl: 'https://hips.hearstapps.com/hmg-prod/images/2022-ford-mustang-shelby-gt500-02-1636734552.jpg' },
     // Add more cars as needed
 ];
 
