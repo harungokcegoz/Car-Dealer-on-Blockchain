1bd454491569c0b7a19e470e9540b51d96d0c88a
Harun Gokcegoz <harungokcegoz92@gmail.com>
2024-03-23 16:19:34 +0100

    IPFS links are displayed correctly.

--- client/src/components/cards/CarCard.tsx
+++ client/src/components/cards/CarCard.tsx
@@ -6,17 +6,16 @@ type CarCardProps = {
     car: Car;
     type: string;
 }
-
 const CarCard = ({ car, type }: CarCardProps) => {
     return (
         <div className="shadow-md rounded overflow-hidden">
-            <Image className="w-full h-80 object-cover" src={car.imageUrl} alt={`${car.brand} Car`} />
+            <Image className="w-full h-80 object-cover" src={car.imageUrl} alt={`${car.brand} Car`} width={300} height={200} />
             <div className="p-4">
                 <h3 className="text-xl font-bold mb-2">{car.brand}</h3>
-                <p className="text-sm text-gray-600 mb-2">{car.licensePlate} - {car.color}</p>
-                <p className="text-sm text-gray-600 mb-2">Seller: {car.owner}</p>
-                <p className="text-sm text-gray-600 mb-2">Chasis Number: {car.chassisNumber}</p>
-                <p className="text-sm text-gray-600 mb-2">Mileage: {String(car.mileage)} km</p>
+                <p className="text-sm text-gray-600 mb-2 uppercase">{car.licensePlate} - {car.color}</p>
+                <p className="text-sm text-gray-600 mb-2 uppercase">Seller: {car.owner}</p>
+                <p className="text-sm text-gray-600 mb-2 uppercase">Chasis Number: {car.chassisNumber}</p>
+                <p className="text-sm text-gray-600 mb-2 uppercase">Mileage: {String(car.mileage)} km</p>
                 <p className="text-xl font-bold text-sky-700">${String(car.askingPrice)}</p>
             </div>
             {type === 'userpage' && 
--- client/src/hooks/useFetchCars.ts
+++ client/src/hooks/useFetchCars.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { initWeb3, callContractMethod } from '../utils/CarContract';
 import { Car } from '../types/CarInterface';
+import { nftStorageApiKey } from '../utils/FetchKeys';
 
 const useCarListings = () => {
     const [initialized, setInitialized] = useState(false);
@@ -18,7 +19,11 @@ const useCarListings = () => {
         const fetchCarListings = async () => {
             try {
                 const fetchedCars = await callContractMethod('getAllCars');
-                setCars(fetchedCars);
+                const carsWithImages = await Promise.all(fetchedCars.map(async (car) => {
+                    const imageUrl = await getImageUrlFromCID(car.imageUrl);
+                    return { ...car, imageUrl };
+                }));
+                setCars(carsWithImages);
             } catch (error) {
                 console.error("Error fetching car listings:", error);
             }
@@ -28,6 +33,20 @@ const useCarListings = () => {
             fetchCarListings();
         }
     }, [initialized]);
+
+    const getImageUrlFromCID = async (cid:string) => {
+        try {
+            const response = await fetch(`https://api.nft.storage/${cid}`, {
+                headers: { Authorization: `Bearer ${nftStorageApiKey}` },
+            });
+            const data = await response.json();
+            const imageName = data.value.files[0].name;
+            return `https://ipfs.io/ipfs/${cid}/${imageName}`;
+        } catch (error) {
+            console.error("Error fetching image from CID:", error);
+            return '';
+        }
+    };
 
     return { initialized, cars };
 };
