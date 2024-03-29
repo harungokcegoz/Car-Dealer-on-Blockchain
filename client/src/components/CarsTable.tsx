5219047557b782982d38d336d7696141ce189052
Harun Gokcegoz <harungokcegoz92@gmail.com>
2024-03-29 18:16:52 +0100

    Cars Table is restyled

--- client/src/components/CarsTable.tsx
+++ client/src/components/CarsTable.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 import Button from './atoms/Button';
 import { Car } from '../types/CarInterface';
 
@@ -15,42 +16,56 @@ const CarTable = ({ title, cars, handler }: CarTableProps) => (
       <p className="text-xl font-bold">There is no result yet.</p>
     ) : (
       <div>
-        <div className="columns flex gap-20 border-b-2 border-orange-300 py-6 drop-shadow-md">
-          <div className="flex-1">
+        <div className="columns grid grid-cols-6 gap-20 border-b-2 border-orange-300 py-6 drop-shadow-md">
+          <div className="flex justify-center">
+            <p className="text-md font-bold">Image</p>
+          </div>
+          <div className="flex justify-center">
             <p className="text-md font-bold">Car Brand</p>
           </div>
-          <div className="flex-1">
+          <div className="flex justify-center">
             <p className="text-md font-bold">License Plate</p>
           </div>
-          <div className="flex-1">
+          <div className="flex justify-center">
             <p className="text-md font-bold">Current Mileage</p>
           </div>
-          <div className="flex-1">
+          <div className="flex justify-center">
             <p className="text-md font-bold">Asking Price</p>
           </div>
           {handler && (
-            <div className="flex-1">
+            <div className="flex justify-center">
               <p className="text-md font-bold">Action</p>
             </div>
           )}
         </div>
         <div className="rows mt-5">
           {cars.map((car, index) => (
-            <div key={index} className="mb-4 flex gap-20 border-b-2 py-7 items-center">
-              <div className="flex-1">
+            <div key={index} className="mb-4 grid grid-cols-6 gap-20 border-b-2 py-7 items-center">
+              <div className="flex justify-center">
+                <span>
+                  <Image
+                    className="rounded"
+                    src={car.imageUrl}
+                    alt="car"
+                    width={120}
+                    height={100}
+                  />
+                </span>
+              </div>
+              <div className="flex justify-center">
                 <p className="text-md ">{car.brand}</p>
               </div>
-              <div className="flex-1">
+              <div className="flex justify-center">
                 <p>{car.licensePlate}</p>
               </div>
-              <div className="flex-1">
+              <div className="flex justify-center">
                 <p>{String(car.mileage)} km</p>
               </div>
-              <div className="flex-1">
+              <div className="flex justify-center">
                 <p>${String(car.askingPrice)}</p>
               </div>
               {handler && (
-                <div className="flex-1">
+                <div className="flex justify-center">
                   <Button text="Confirm" onClick={() => handler(car.carId)} type="submit" />
                 </div>
               )}
